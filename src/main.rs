aa3348de7dc4f3969905e0d6004b4e68b68e4f90
Joseph Guhlin <joseph.guhlin@gmail.com>
2023-10-06 09:08:12 +1300

    .

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,9 @@
-use ratatui::{backend::CrosstermBackend, Terminal};
 use std::io;
 
+use ratatui::{backend::CrosstermBackend, Terminal};
+use fffx::*;
+
 fn main() {
-    use crossterm::{
-        event::{self, DisableMouseCapture, EnableMouseCapture},
-        execute,
-        terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
-    };
-    use ratatui::{
-        backend::CrosstermBackend,
-        widgets::{Block, Borders},
-        Terminal,
-    };
-    use std::{io, thread, time::Duration};
 
 }
 
